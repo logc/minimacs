6c02030629f01de08be81ad5e226a040aa97d299
Luis Osa <luis.osa.gdc@gmail.com>
2022-09-29 15:46:19 +0200

    Switch java version interactive
    Copied from https://blog.brunobonacci.com/2020/07/02/switching-between-multiple-jdk-in-emacs/

--- init.el
+++ init.el
@@ -277,6 +277,61 @@
   (lsp-mode . dap-mode)
   (lsp-mode . dap-ui-mode))
 
+;; Java
+
+;; switch java
+
+(setq JAVA_BASE "/Library/Java/JavaVirtualMachines")
+
+(defun switch-java--versions ()
+  "Return the list of installed JDK."
+  (seq-remove
+   (lambda (a) (or (equal a ".") (equal a "..")))
+   (directory-files JAVA_BASE)))
+
+(defun switch-java--save-env ()
+  "Store original PATH and JAVA_HOME."
+  (when (not (boundp 'SW_JAVA_PATH))
+    (setq SW_JAVA_PATH (getenv "PATH")))
+  (when (not (boundp 'SW_JAVA_HOME))
+    (setq SW_JAVA_HOME (getenv "JAVA_HOME"))))
+
+(defun switch-java ()
+  "List the installed JDKs and enable to switch the JDK in use."
+  (interactive)
+  ;; store original PATH and JAVA_HOME
+  (switch-java--save-env)
+
+  (let ((ver (completing-read
+              "Which Java: "
+              (seq-map-indexed
+               (lambda (e i) (list e i)) (switch-java--versions))
+              nil t "")))
+    ;; switch java version
+    (setenv "JAVA_HOME" (concat JAVA_BASE "/" ver "/Contents/Home"))
+    (setenv "PATH" (concat (concat (getenv "JAVA_HOME") "/bin/java")
+                           ":" SW_JAVA_PATH)))
+  ;; show version after switching
+  (switch-java-which-version?))
+
+(defun switch-java-default ()
+  "Restore the default Java version."
+  (interactive)
+  ;; store original PATH and JAVA_HOME
+  (switch-java--save-env)
+
+  ;; switch java version
+  (setenv "JAVA_HOME" SW_JAVA_HOME)
+  (setenv "PATH" SW_JAVA_PATH)
+  ;; show version
+  (switch-java-which-version?))
+
+(defun switch-java-which-version? ()
+  "Display the current version selected Java version."
+  (interactive)
+  ;; displays current java version
+  (message (concat "Java HOME: " (getenv "JAVA_HOME"))))
+
 ;; Magit
 (use-package magit)
 
