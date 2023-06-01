35651c9181a5d24616dcd7ceddc4785fbcb42664
Luis Osa <luis.osa.gdc@gmail.com>
2023-06-01 21:15:20 +0200

    Set path and library path in early init

--- early-init.el
+++ early-init.el
@@ -1,16 +1,6 @@
 ;; Disable package.el in favor of straight.el
 (setq package-enable-at-startup nil)
 
-;; HACK Work around native compilation on macOS failing with 'ld: library not
-;; found for -lemutls_w'.
-;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554
-(setenv "LIBRARY_PATH"
-	(string-join
-	 '("/opt/homebrew/opt/gcc/lib/gcc/13"
-	   "/opt/homebrew/opt/libgccjit/lib/gcc/13"
-	   "/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin22/13")
-	 ":"))
-
 (defmacro with-gc-cons-threshold (threshold &rest body)
   "Execute BODY with GC threshold temporarily set to THRESHOLD.
    Log the current GC threshold before and after the block is executed."
@@ -23,3 +13,28 @@
        (setq gc-cons-threshold ,orig-gc-threshold)
        (message "Restored GC threshold: %s" gc-cons-threshold))))
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
+;; HACK Work around native compilation on macOS failing with 'ld: library not
+;; found for -lemutls_w'.
+;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554
+(setenv "LIBRARY_PATH"
+	(string-join
+	 '("/opt/homebrew/opt/gcc/lib/gcc/13"
+	   "/opt/homebrew/opt/libgccjit/lib/gcc/13"
+	   "/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin22/13")
+	 ":"))
