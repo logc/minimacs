bf582bd902cc569cdafbbb4f84636ae6289a8fcc
Luis Osa <luis.osa.gdc@gmail.com>
2023-06-01 21:16:12 +0200

    Set GPG program from Homebrew

--- init.el
+++ init.el
@@ -30,7 +30,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" "8701800a33d6afe1588af6383c8deb7847b7fdfb8c7b134753a0b0f26524c967" "0a1b7b2d4e8931c14e40d87ac58d1e20624307d9a90643e6c96d7aaeb597762b" "34af44a659b79c9f92db13ac7776b875a8d7e1773448a8301f97c18437a822b6" "d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563" default)))
+   '("835d934a930142d408a50b27ed371ba3a9c5a30286297743b0d488e94b225c5f" "4c7228157ba3a48c288ad8ef83c490b94cb29ef01236205e360c2c4db200bb18" "8701800a33d6afe1588af6383c8deb7847b7fdfb8c7b134753a0b0f26524c967" "0a1b7b2d4e8931c14e40d87ac58d1e20624307d9a90643e6c96d7aaeb597762b" "34af44a659b79c9f92db13ac7776b875a8d7e1773448a8301f97c18437a822b6" "d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563" default))
+ '(epg-gpg-program "/opt/homebrew/bin/gpg"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
