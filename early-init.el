c0fa687eb9cf99ae06665886ebb7b31d7171eff3
Luis Osa <luis.osa.gdc@gmail.com>
2023-10-07 21:07:35 +0200

    Fix shell to make path work in terminal
    The $SHELL env variable points to the current shell if Emacs is
    started from terminal, but it points to the default shell when started
    as a GUI program. The path was not set correctly for the terminal
    version.
    
    This solution is not portable but it works.

--- early-init.el
+++ early-init.el
@@ -23,7 +23,7 @@ apps are not started from a shell."
   (interactive)
   (let ((path-from-shell (replace-regexp-in-string
 			  "[ \t\n]*$" "" (shell-command-to-string
-					  "$SHELL --login -c 'echo $PATH'"
+					  "/bin/zsh --login -c 'echo $PATH'"
 						    ))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
