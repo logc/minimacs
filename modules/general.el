7aa2735d1a2416caa041bf712a1a92063b66619a
Luis Osa <luis.osa.gdc@gmail.com>
2024-03-12 09:40:23 +0100

    Change theme to tomorrow-night

--- modules/general.el
+++ modules/general.el
@@ -80,24 +80,10 @@
 
 ;; Theme
 
-;(use-package doom-themes
-;  :ensure t
-;  :config
-;  ;; Global settings (defaults)
-;  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;
-;  ;; Enable flashing mode-line on errors
-;  (doom-themes-visual-bell-config)
-;
-;  ;; Corrects (and improves) org-mode's native fontification.
-;  (doom-themes-org-config))
-
-(use-package ef-themes
+(use-package color-theme-sanityinc-tomorrow
   :ensure t
   :init
-  (load-theme 'ef-elea-dark t)
-  (setq ef-themes-to-toggle '(ef-elea-dark ef-elea-light)))
+  (load-theme 'sanityinc-tomorrow-eighties))
 
 (use-package solaire-mode
   :ensure t
